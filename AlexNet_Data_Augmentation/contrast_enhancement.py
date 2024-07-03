e74c477411012312c21d3022a0cc5b71b6980c54
valentinaserbanescu <valentinaserbanescu2000@yahoo.com>
2024-07-03 18:42:18 +0200

    Because of wrong branch I delete everthing Valentina

--- AlexNet_Data_Augmentation/contrast_enhancement.py
+++
@@ -1,9 +0,0 @@
-from PIL import ImageEnhance
-class ContrastEnhancement(object):
-    def __init__(self, factor=1.5):
-         self.factor = factor
-
-    def __call__(self, img):
-         enhancer = ImageEnhance.Contrast(img)
-         img = enhancer.enhance(self.factor)
-         return img
